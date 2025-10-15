ea910e4027a8a9dfad42ec95befe35ee330e4cfc
LukeyW11 <luke1125w@gmail.com>
2025-10-15 07:48:46 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "100px" }}>
+      <h1 style={{ color: "royalblue" }}>Welcome to Governance!</h1>
+      <p>This is the UK Parliament simulation starter.</p>
+    </div>
+  );
+}
